339b00da7c98c05da69eb62065e2627741229ee4
Vo Dinh Lan <vodinhlanqn@gmail.com>
2023-01-04 15:04:39 +0700

    Code NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function NotFound() {
   return (
-    <>
-      <div>NotFound</div>
-      <div>NotFound</div>
-    </>
+    <div className='relative'>
+      <img className='mx-auto h-[100vh] object-cover' src="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif"
+        alt="https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif" />
+      <h2 className='uppercase absolute bottom-[20%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[12px] md:text-[20px] font-bold'>Không tìm thấy trang</h2>
+    </div>
   )
 }
